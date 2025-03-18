34a1ee65cf2a6c76df1037e1cb6a76683dc046ef
apjgaming <lewiand1234@gmail.com>
2025-03-18 16:47:21 -0700

    build fix?

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "src/",
+  plugins: [react()],
   build: {
     outDir: "../dist",
     rollupOptions: {
